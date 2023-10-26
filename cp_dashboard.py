54afdb028cc7d93f0c2fe9bcc868faef01832c0a
unknown <kellytheng.tkl@gmail.com>
2023-10-26 22:02:22 +0800

    commented topic modelling

--- cp_dashboard.py
+++ cp_dashboard.py
@@ -278,163 +278,103 @@ with tab1:
 
     #########################################################
 
-    # make 2 columns for second row of dashboard
-    # col10, col11, col12 = st.columns([45, 10, 45])
 
-    # def get_top_n_gram(filtered_data, ngram_range, n=10):
-    #     # load the corpus and vectorizer
-    #     corpus = filtered_data['text']
-    #     vectorizer = CountVectorizer(
-    #         analyzer="word", ngram_range=ngram_range
-    #     )
+# with tab2:
+#     st.subheader('Topic Modelling')
 
-    #     # use the vectorizer to count the n-grams frequencies
-    #     X = vectorizer.fit_transform(corpus.astype(str).values)
-    #     words = vectorizer.get_feature_names_out()
-    #     words_count = np.ravel(X.sum(axis=0))
+#     # Using Zero-shot classification
+#     # Initialize the zero-shot classification pipeline
 
-    #     # store the results in a dataframe
-    #     df = pd.DataFrame(zip(words, words_count))
-    #     df.columns = ["words", "counts"]
-    #     df = df.sort_values(by="counts", ascending=False).head(n)
-    #     df["words"] = df["words"].str.title()
-    #     return df
-
-    # def plot_n_gram(n_gram_df, title, color="#54A24B"):
-    #     # plot the top n-grams frequencies in a bar chart
-    #     fig = px.bar(
-    #         x=n_gram_df.counts,
-    #         y=n_gram_df.words,
-    #         title="<b>{}</b>".format(title),
-    #         text_auto=True,
-    #     )
-    #     fig.update_layout(plot_bgcolor="white")
-    #     fig.update_xaxes(title=None)
-    #     fig.update_yaxes(autorange="reversed", title=None)
-    #     fig.update_traces(hovertemplate="<b>%{y}</b><br>Count=%{x}", marker_color=color)
-    #     return fig
-
-    # with col10:
-    #     # plot the top 10 occuring words 
-    #     top_unigram = get_top_n_gram(filtered_data, ngram_range=(1, 1), n=10)
-    #     unigram_plot = plot_n_gram(
-    #         top_unigram, title="Top 10 Occuring Words"
-    #     )
-    #     unigram_plot.update_layout(height=350)
-    #     st.plotly_chart(unigram_plot, use_container_width=True)
-
-    # with col12:
-    #     top_bigram = get_top_n_gram(filtered_data, ngram_range=(2, 2), n=10)
-    #     bigram_plot = plot_n_gram(
-    #         top_bigram, title="Top 10 Occuring Bigrams"
-    #     )
-    #     bigram_plot.update_layout(height=350)
-    #     st.plotly_chart(bigram_plot, use_container_width=True)
-
-    #########################################################
-
-
-with tab2:
-    st.subheader('Topic Modelling')
-
-    # Using Zero-shot classification
-    # Initialize the zero-shot classification pipeline
-
-    @st.cache_data
-    def get_zero_shot_model():
-        # # Check if the selected language is Chinese
-        # if language.lower() == "chinese":
-        #     return pipeline('zero-shot-classification', model='joeddav/xlm-roberta-large-xnli') #cant access without permission
-        # else:
-        #     return pipeline('zero-shot-classification', model='joeddav/distilbert-base-uncased-agnews-student')
+#     @st.cache_data
+#     def get_zero_shot_model():
+#         # # Check if the selected language is Chinese
+#         # if language.lower() == "chinese":
+#         #     return pipeline('zero-shot-classification', model='joeddav/xlm-roberta-large-xnli') #cant access without permission
+#         # else:
+#         #     return pipeline('zero-shot-classification', model='joeddav/distilbert-base-uncased-agnews-student')
         
-        return pipeline('zero-shot-classification', model='joeddav/distilbert-base-uncased-agnews-student')
-        # return pipeline('zero-shot-classification', model='joeddav/xlm-roberta-large-xnli', use_auth_token='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')
+#         return pipeline('zero-shot-classification', model='joeddav/distilbert-base-uncased-agnews-student')
+#         # return pipeline('zero-shot-classification', model='joeddav/xlm-roberta-large-xnli', use_auth_token='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')
 
-    with st.spinner("Downloading zero shot classifier"):
-      classifier = get_zero_shot_model()
+#     with st.spinner("Downloading zero shot classifier"):
+#       classifier = get_zero_shot_model()
     
-    # labels = ['retail', 'food', 'facilities']
-    labels = ['car park', 'food', 'environment','services','price','furniture']
-    # labels = ['car park', 'food', 'environment', 'retail']
+#     # labels = ['retail', 'food', 'facilities']
+#     labels = ['car park', 'food', 'environment','services','price','furniture']
+#     # labels = ['car park', 'food', 'environment', 'retail']
 
-    # # Function to predict categories and add them to the DataFrame
-    # def predict_categories(text):
-    #     result = classifier(text, labels, multi_class_True)
-    #     predicted_category = result['labels'][0]
-    #     return predicted_category
+#     start_modelling_time = datetime.now()
+#     st.write(start_modelling_time)
 
-    start_modelling_time = datetime.now()
-    st.write(start_modelling_time)
-
-    @st.cache_data 
-    def predict_category(text):
-        return  text.apply(lambda x: classifier(x, labels)['labels'][0])
-    with st.spinner('Building topic modelling'):
-        # Apply the predict_categories function to all rows in the dataset
-        # filtered_data['Predicted Category'] = filtered_data['text'].apply(predict_categories)
+#     @st.cache_data 
+#     def predict_category(text):
+#         return  text.apply(lambda x: classifier(x, labels)['labels'][0])
     
-        filtered_data['Predicted Category'] = predict_category(filtered_data['text_short'])
+#     with st.spinner('Building topic modelling'):
+#         # Apply the predict_categories function to all rows in the dataset
+#         # filtered_data['Predicted Category'] = filtered_data['text'].apply(predict_categories)
+    
+#         filtered_data['Predicted Category'] = predict_category(filtered_data['text_short'])
                                                     
-        # Display the dataset with the predicted categories
-        selected_labels = st.multiselect("Select store:", options=labels, default = labels)
+#         # Display the dataset with the predicted categories
+#         selected_labels = st.multiselect("Select store:", options=labels, default = labels)
 
-        st.write("Predicted Categories for Each Text:")
-        filtered_data_class = filtered_data[filtered_data['Predicted Category'].isin(selected_labels)]
-        st.write(filtered_data_class[['text_short', 'Predicted Category', 'sent_res']])
+#         st.write("Predicted Categories for Each Text:")
+#         filtered_data_class = filtered_data[filtered_data['Predicted Category'].isin(selected_labels)]
+#         st.write(filtered_data_class[['text_short', 'Predicted Category', 'sent_res']])
 
-        category_counts = filtered_data['Predicted Category'].value_counts()
-        category_counts_sorted = category_counts.sort_values(ascending=False)
+#         category_counts = filtered_data['Predicted Category'].value_counts()
+#         category_counts_sorted = category_counts.sort_values(ascending=False)
 
-        plot_category = px.bar(filtered_data, x=category_counts_sorted.values, y=category_counts_sorted.index, orientation='h')
-        st.plotly_chart(plot_category, use_container_width=True)
-    end_modelling_time = datetime.now()
+#         plot_category = px.bar(filtered_data, x=category_counts_sorted.values, y=category_counts_sorted.index, orientation='h')
+#         st.plotly_chart(plot_category, use_container_width=True)
+#     end_modelling_time = datetime.now()
 
-    st.write(end_modelling_time - start_modelling_time)
+#     st.write(end_modelling_time - start_modelling_time)
 
-    def generate_word_cloud(text, title, font_path=None):
-        st.subheader(title)
-        wordcloud = WordCloud(
-            background_color='white',
-            font_path=font_path,
-        ).generate(text)
+#     def generate_word_cloud(text, title, font_path=None):
+#         st.subheader(title)
+#         wordcloud = WordCloud(
+#             background_color='white',
+#             font_path=font_path,
+#         ).generate(text)
 
-        # Create a figure for the word cloud and display it
-        wc_figure, ax = plt.subplots(figsize=(10, 5))
-        ax.imshow(wordcloud, interpolation='bilinear')
-        ax.axis('off')
-        return wc_figure
+#         # Create a figure for the word cloud and display it
+#         wc_figure, ax = plt.subplots(figsize=(10, 5))
+#         ax.imshow(wordcloud, interpolation='bilinear')
+#         ax.axis('off')
+#         return wc_figure
     
-    # Find the top category labels if category counts is more than 100 else word cloud won't be able to display
-    # top_labels = category_counts.index[:2]
-    top_labels = category_counts[category_counts >= 100].index
+#     # Find the top category labels if category counts is more than 100 else word cloud won't be able to display
+#     # top_labels = category_counts.index[:2]
+#     top_labels = category_counts[category_counts >= 100].index
 
-    # Generate and display word clouds for positive and negative sentiments by looping through the labels and create word clouds
-    for label in top_labels:
-        st.subheader(f'Word Clouds for {label} in {language}')
+#     # Generate and display word clouds for positive and negative sentiments by looping through the labels and create word clouds
+#     for label in top_labels:
+#         st.subheader(f'Word Clouds for {label} in {language}')
         
-        # Filter the data for the current label
-        label_data = filtered_data_class[filtered_data_class['Predicted Category'] == label]
+#         # Filter the data for the current label
+#         label_data = filtered_data_class[filtered_data_class['Predicted Category'] == label]
 
-        # Separate positive and negative sentiments
-        positive_text = " ".join(label_data[label_data['sent_res'] == 'positive']['text_short'])
-        negative_text = " ".join(label_data[label_data['sent_res'] == 'negative']['text_short'])
+#         # Separate positive and negative sentiments
+#         positive_text = " ".join(label_data[label_data['sent_res'] == 'positive']['text_short'])
+#         negative_text = " ".join(label_data[label_data['sent_res'] == 'negative']['text_short'])
 
-        # Preprocess the text for the word clouds
-        preprocessed_positive_text = preprocess_text(positive_text)
-        preprocessed_negative_text = preprocess_text(negative_text)
+#         # Preprocess the text for the word clouds
+#         preprocessed_positive_text = preprocess_text(positive_text)
+#         preprocessed_negative_text = preprocess_text(negative_text)
 
-        # Generate and display the word clouds for positive and negative sentiments
-        # Create two columns for positive and negative word clouds
-        col1, col2 = st.columns(2)
+#         # Generate and display the word clouds for positive and negative sentiments
+#         # Create two columns for positive and negative word clouds
+#         col1, col2 = st.columns(2)
 
-        with col1:
-            positive_wc_figure = generate_word_cloud(preprocessed_positive_text, f'Positive', font_path=font_path)
-            st.pyplot(positive_wc_figure)
+#         with col1:
+#             positive_wc_figure = generate_word_cloud(preprocessed_positive_text, f'Positive', font_path=font_path)
+#             st.pyplot(positive_wc_figure)
 
-        with col2:
-            negative_wc_figure = generate_word_cloud(preprocessed_negative_text, f'Negative', font_path=font_path)
-            st.pyplot(negative_wc_figure)
+#         with col2:
+#             negative_wc_figure = generate_word_cloud(preprocessed_negative_text, f'Negative', font_path=font_path)
+#             st.pyplot(negative_wc_figure)
 
     #########################################################
 
